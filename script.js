e4cd76adf0425752432c3ab4d4ecdfda24b0ca3b
Julie Lam <43072938+blouhie@users.noreply.github.com>
2021-05-25 16:27:47 -0400

    slight changes

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ if (window.location.pathname === "/") {
 
   sessionStorage.setItem("keyOrder", keyOrder);
   sessionStorage.setItem("promptOrder", promptOrder);
-
-  console.log(window.sessionStorage);
 }
 
 // prompt page interactivity
@@ -36,6 +34,7 @@ if (window.location.pathname === "/views/prompt.html") {
   let idx = parseInt(promptOrder.pop(), 10);
   let currentPrompt = PROMPT_CARDS[idx];
   sessionStorage.answer = currentPrompt.answer;
+  sessionStorage.category = currentPrompt.category;
 
   if (promptOrder.length === 0) {
     promptOrder = shuffle(PROMPT_CARDS);
@@ -51,18 +50,5 @@ if (window.location.pathname === "/views/prompt.html") {
 // answer page interactivity
 if (window.location.pathname === "/views/answer.html") {
   document.querySelector("p").innerHTML = sessionStorage.answer;
+  document.querySelector("h4").innerHTML = sessionStorage.category;
 }
-
-/**
- *
- * save to session storage as space-delineated string
- *
- * upon accessing
- * grab string
- * turn into list (separate by spaces)
- * pop off last item
- * 	if list empty
- * 	shuffle again
- * turn into string
- * save to session storage as space-delinated string
- */
