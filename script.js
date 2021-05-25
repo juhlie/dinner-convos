24f2f5da8db8d0d979f41e9ac816f4a6d797b724
Julie Lam <43072938+blouhie@users.noreply.github.com>
2021-05-25 16:19:53 -0400

    added interactivity

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { KEY_CARDS, PROMPT_CARDS } from "/cards.js";
 
-const shuffle = (deck) => {
+export const shuffle = (deck) => {
   const order = [];
   for (let i = 0; i < deck.length; i++) {
     order.push(i);
@@ -18,5 +18,51 @@ const shuffle = (deck) => {
   return order;
 };
 
-let keyOrder = shuffle(KEY_CARDS);
-let promptOrder = shuffle(PROMPT_CARDS);
+// main page interactivity
+if (window.location.pathname === "/") {
+  let keyOrder = shuffle(KEY_CARDS).join(" ");
+  let promptOrder = shuffle(PROMPT_CARDS).join(" ");
+
+  sessionStorage.setItem("keyOrder", keyOrder);
+  sessionStorage.setItem("promptOrder", promptOrder);
+
+  console.log(window.sessionStorage);
+}
+
+// prompt page interactivity
+if (window.location.pathname === "/views/prompt.html") {
+  let promptOrder = sessionStorage.promptOrder.split(" ");
+
+  let idx = parseInt(promptOrder.pop(), 10);
+  let currentPrompt = PROMPT_CARDS[idx];
+  sessionStorage.answer = currentPrompt.answer;
+
+  if (promptOrder.length === 0) {
+    promptOrder = shuffle(PROMPT_CARDS);
+  }
+
+  sessionStorage.promptOrder = promptOrder.join(" ");
+
+  document.querySelector("h2").innerHTML = currentPrompt.title;
+  document.querySelector("p").innerHTML = currentPrompt.prompt;
+  document.querySelector("h4").innerHTML = currentPrompt.category;
+}
+
+// answer page interactivity
+if (window.location.pathname === "/views/answer.html") {
+  document.querySelector("p").innerHTML = sessionStorage.answer;
+}
+
+/**
+ *
+ * save to session storage as space-delineated string
+ *
+ * upon accessing
+ * grab string
+ * turn into list (separate by spaces)
+ * pop off last item
+ * 	if list empty
+ * 	shuffle again
+ * turn into string
+ * save to session storage as space-delinated string
+ */
