3f2bf90f69a5a7b01b53498c85899f2e0598af61
Julie Lam <43072938+blouhie@users.noreply.github.com>
2021-05-25 15:20:56 -0400

    converted back to static site

---
+++ script.js
@@ -0,0 +1,22 @@
+import { KEY_CARDS, PROMPT_CARDS } from "/cards.js";
+
+const shuffle = (deck) => {
+  const order = [];
+  for (let i = 0; i < deck.length; i++) {
+    order.push(i);
+  }
+
+  let currentIdx = order.length;
+  while (0 !== currentIdx) {
+    const randomIdx = Math.floor(Math.random() * currentIdx);
+    currentIdx--;
+    const temp = order[currentIdx];
+    order[currentIdx] = order[randomIdx];
+    order[randomIdx] = temp;
+  }
+
+  return order;
+};
+
+let keyOrder = shuffle(KEY_CARDS);
+let promptOrder = shuffle(PROMPT_CARDS);
