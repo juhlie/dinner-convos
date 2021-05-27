f4c13a9d0d1eb3a5ba05df4715967a819c195898
Julie Lam <43072938+blouhie@users.noreply.github.com>
2021-05-27 01:32:40 -0400

    information tab + scrollbars for all text overflow

--- views.js
+++ views.js
@@ -1,39 +1,138 @@
 export const about = `
+<div class="content">
 <h3>About</h3>
 
 <p>
-	In 1994, 12 black women coined the term reproductive justice. It is "the
-	complete physical, mental, spiritual, political, social, and economic
-	well-being of women and girls, based on the full achievement and
-	protection of women’s human rights" as Loretta Ross describes. Fighting
-	against reproductive oppression and misinformation is essential for our
-	liberation.
+	In 1994, 12 black women coined the term “reproductive justice,” or "the complete physical, mental, spiritual, political, social, and economic well-being of women and girls, based on the full achievement and protection of women’s human rights" as Loretta Ross describes. Fighting against reproductive oppression and misinformation is essential for our liberation.
 </p>
 
 <p>
-	Perfect Conversations for Every Dinner Table is a resource that aims to
-	support the power of individuals to provoke open and healthy discussions
-	on how to achieve better lives for their families and their communities.
-	Start a meaningful and shameless conversation by picking a card.
-</p>`;
+	In 2021, 6 women of color were brought together by their shared interest in reproductive justice at the Feminist Future(s) Hackathon—Tia, Julie, Krithika, Dina, Althea and Tanvi. We believe in the power of healthy conversations to plant seeds for change. Conversations for Every Dinner  Table is a resource that reframes traditionally taboo and stigmatized topics related to reproductive processes so we can feel comfortable talking about them openly, learn from one another’s lived experiences, and have fun. By the end of the game, we hope that players can feel more connected with their own bodies as well as others’, more equipped to step outside of the oppressive narrative framework, and more confident in defining our own criteria of normalcy and care.
+</p>
 
-export const answer = `
-<h2>ANSWER</h2>
-<p></p>
-<h4></h4>`;
+<p>
+  Start a meaningful conversation by picking a card.
+</p>
+</div>
+`;
+
+export const background = `
+<div class="content">
+<h3>DINA</h3>
+
+<p>
+Dina Benbrahim (she/her/hers) is an Arab multidisciplinary creative who uses a feminist lens to focus on illuminating the power in human beings to be transformative forces in society. Her current research investigates d=esign for civic action and social justice for marginalized communities to collectively reimagine equitable futures. Dina has experience in design, art direction, copywriting and entrepreneurship in New York and Casablanca, with SYPartners New York, J. Walter Thompson New York and Casablanca, and Shem’s Publicité. She is now an Assistant Professor in Design. She is motivated to activate greatness in everyone she encounters.
+</p>
+
+<h3>TANVI</h3>
+
+<p>
+Tanvi Avasthi (she/her/hers is a nurse, soon-to-be-midwife-and-women’-health-nurse-practitioner, and anti-racism organizer. She grew up in Virginia and has spent the past few years in Baltimore learning to love crabs. Tanvi loves teaching nursing students and yoga students, guiding people through the best sex and birth possible, and crushing white supremacy and patriarchy unapologetically.
+</p>
+
+<h3>TIA</h3>
+
+<p>
+Tia Jain (she/her/hers) is an incoming freshman at UC Berkeley studying Computer Science. She is deeply interested in solving issues with an ethical lens—especially in technology. For the past couple of years, she has been exploring the intersection of Artificial Intelligence and Healthcare by publishing bioinformatics and machine learning research as well as an AI-based iOS app. She’s committed to empowering and amplifying the voices of historically underrepresented students, particularly in the CS space.
+</p>
+
+<h3>ALTHEA</h3>
+
+<p>
+Althea Rao (she/her/hers) creates social engagement models to facilitate playful conversations that unpack normalized systemic power imbalance. She draws influence from media studies, intersectionality, design justice and grass-root organizing. Her project Vagina Chorus, a bio-responsive, socially-engaged multimedia performance spotlighting women’s health and healthcare accessibilities, is a recipient of the 2020 MAP Fund. Currently, she focuses on revealing states of institutionalized disempowerment, facilitating radical community healing, and designing systems for intrinsic accountability.
+</p>
+
+<h3>JULIE CHO</h3>
+
+<h3>JULIE LAM</h3>
+
+  
+</p>
+</div>`;
 
 export const instructions = `
+<div class="content">
 <h3>Instructions</h3>
-<p>TODO: add stuff about prompt cards, key cards, etc</p>`;
+
+<p>
+As the title suggests, Conversations for Every Dinner Table is suitable for intimate or social group gatherings. Conversations may take place among groups of 2 - 7 people of friends and families, between generations, and across genders. With the presence of a mediator, this game can be used to facilitate educational public programs, such as school workshops, public library offerings, or Trivia Night at local bars, using the digital version (currently work-in-progress). If there is a use case you’d like to suggest or discuss with us, please feel free to send an email to ffrj2021@googlegroups.com. 
+</p>
+
+<p>
+There are 2 types of cards to draw from in this deck: Prompt Cards and Key Cards. First, separate the Prompt Cards and Key Cards into 2 separate decks.
+</p>
+
+<h3>PROMPT CARDS</h3>
+
+<p>
+Prompt Cards invite responses from players, starting a conversation. They are divided into seven topic categories: 
+</p>
+
+<p>
+Postpartum | Menstruation | Pregnancy | Menopause | Sex | Abortion | Reproductive health 
+</p>
+
+<h3>KICKING OFF THE GAME</h3>
+
+<p>
+At the beginning of the game, each player draws seven Prompt Cards, one from each category. Alternatively, for a quicker game, shuffle all the categories together and ask each player to draw only four Prompt Cards. 
+</p>
+
+<p>
+Any player can volunteer to kick off the game by picking one of their cards and reading the prompt written on the card out loud to the group. Some prompts will ask the player to answer a question, while others may instruct the player to draw something or share a story. Below are the types of prompts:
+</p>
+
+<p>
+Answer a Question | Reflect | Guess | Think Wildly | Draw | Tell A Story
+</p>
+
+<p>
+The player will respond to the prompt first, then invite other players from the group to respond to it, creating a conversation. 
+</p>
+
+<h3>KEY CARDS</h3>
+<p>
+If a player finds it difficult to come up with an answer or the conversation experiences hiccups, draw a Key Card to help move the dialogue forward. They are:
+</p>
+
+<p>
+Mirror | Silence | Swap | Speculative | Affirmation 
+<br>
+Think Positively | Listen Deeply | Sit In Stillness
+</p>
+
+<h3>MOVING ON</h3>
+
+<p>
+Once all the players agree to move on from the first Prompt Card, the next player (your choice of clockwise, counterclockwise, or popcorn) will continue by reading out one card of their choice.
+</p>
+
+<h3>EXIT</h3>
+
+<p>
+You do not have to finish discussing all the cards in order for this game to end. Like other dinner table conversations, this one is not rigid either.
+</p>
+</div>`;
 
 export const key = `
 <h2></h2>
+<div class="card-content">
 <img></img>
+</div>
 <p></p>
 `;
 
 export const prompt = `
 <h2></h2>
+<div class="card-content">
 <p></p>
+</div>
 <h4></h4>
 `;
+
+export const answer = `
+<h2>ANSWER</h2>
+<div class="card-content">
+<p></p>
+</div>
+<h4></h4>`;
